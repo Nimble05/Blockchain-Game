431dfbc612c214248bab7b9aad93a6edc7bd25c7
Nimble05 <lkj75513@gmail.com>
2026-06-02 14:43:45 +0800

    update config files and index.html

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Blockchain-Game/', 
+  base: '/Blockchain-Game/',
   server: { port: 5173 },
 });
