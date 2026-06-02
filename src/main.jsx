9222cae39a20f2335c04ffc8e7d24b8dd8e90394
Nimble05 <lkj75513@gmail.com>
2026-06-02 13:52:20 +0800

    feat: add 404 page, update vite config and main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Blockchain-Game">
       <App />
     </BrowserRouter>
   </React.StrictMode>
